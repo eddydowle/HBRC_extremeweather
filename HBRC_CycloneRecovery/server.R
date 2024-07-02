c737599a6d9aab29e909b590344bd43723d433d2
Eddy Dowle <dowed34p@MacBook-Pro-8.local>
2024-07-02 21:29:14 +1200

    needs final check but map app should be okay

--- HBRC_CycloneRecovery/server.R
+++ HBRC_CycloneRecovery/server.R
@@ -42,7 +42,6 @@ library('microbiome')
 #test<-left_join(records_eukaryote_freshwater,meta_data,by='UID')
 
 
-# Define server logic required to draw a histogram
 function(input, output, session) {
   
  output$secondSelection<-renderUI({selectInput("taxa", "Select taxa", choices =sort((test %>% select(input$level)  %>% unique()) [,1]))})
@@ -65,9 +64,63 @@ function(input, output, session) {
            year2023_all_sites<-test %>% filter(Year==2023 ) %>% select(HBRC_Site_Name) %>% distinct()
            df<-year2023_all_sites %>% mutate(present = case_when(HBRC_Site_Name %in% year2023_filtered$HBRC_Site_Name~"present_2023",TRUE ~ 'Absent'))         }
          if (input$integer == 2024) {
-           year2024_filtered<-test_filtered_select %>% filter(Year==2021 ) %>% select(HBRC_Site_Name) %>% distinct()
+           year2024_filtered<-test_filtered_select %>% filter(Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
            year2024_all_sites<-test %>% filter(Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
            df<-year2024_all_sites %>% mutate(present = case_when(HBRC_Site_Name %in% year2024_filtered$HBRC_Site_Name~"present_2024",TRUE ~ 'Absent'))         }
+    if (input$integer == "Precyclone (2021 to March 2023) vs Post cyclone (March 2023 to 2024)") {
+      pre_filtered<-test_filtered_select %>% filter(Year==2021|Year==2022 ) %>% select(HBRC_Site_Name) %>% distinct()
+    post_filtered<-test_filtered_select %>% filter(Year==2023|Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
+    pre_all_sites<-test %>% filter(Year==2021|Year==2022 ) %>% select(HBRC_Site_Name) %>% distinct()
+    post_all_sites<-test %>% filter(Year==2023|Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
+    pre_df<-pre_all_sites %>% mutate(present_pre = case_when(HBRC_Site_Name %in% pre_filtered$HBRC_Site_Name~"present_pre",TRUE ~ 'absent_pre'))
+    post_df<-post_all_sites %>% mutate(present_post = case_when(HBRC_Site_Name %in% post_filtered$HBRC_Site_Name~"present_post",TRUE ~ 'absent_post'))
+    df<-full_join(pre_df,post_df,by='HBRC_Site_Name')
+    df<-df %>% mutate(present = case_when(
+      (present_pre == 'absent_pre' & present_post == 'absent_post') ~ 'Absent_pre&post',
+      (present_pre == 'present_pre' & present_post == 'present_post') ~ 'Present_pre&post',
+      (present_pre == 'present_pre' & present_post == 'absent_post') ~ 'Loss post',
+      (present_pre == 'absent_pre' & present_post == 'present_post') ~ 'Gain post', TRUE ~ NA_character_
+    ))
+    #this results in some NA's as there is some sites (2 that are only sampled post)
+    df<-df %>% filter(!HBRC_Site_Name=='Wharerangi - Dan and Lindsay Bates') %>% filter(!HBRC_Site_Name=='Mangaone Rv at Dartmoor Br')
+    }
+    #"2022 vs 2023 (Post cyclone)"
+    if (input$integer == "2022 vs 2023 (Post cyclone)") {
+      filtered_2022<-test_filtered_select %>% filter(Year==2022 ) %>% select(HBRC_Site_Name) %>% distinct()
+      filtered_2023<-test_filtered_select %>% filter(Year==2023) %>% select(HBRC_Site_Name) %>% distinct()
+      all_sites_2022<-test %>% filter(Year==2022) %>% select(HBRC_Site_Name) %>% distinct()
+      all_sites_2023<-test %>% filter(Year==2023) %>% select(HBRC_Site_Name) %>% distinct()
+      pre_df<-all_sites_2022 %>% mutate(present_pre = case_when(HBRC_Site_Name %in% filtered_2022$HBRC_Site_Name~"present_2022",TRUE ~ 'absent_2022'))
+      post_df<-all_sites_2023 %>% mutate(present_post = case_when(HBRC_Site_Name %in% filtered_2023$HBRC_Site_Name~"present_2023",TRUE ~ 'absent_2023'))
+      df<-full_join(pre_df,post_df,by='HBRC_Site_Name')
+      df<-df %>% mutate(present = case_when(
+        (present_pre == 'absent_2022' & present_post == 'absent_2023') ~ 'Absent_2022&2023',
+        (present_pre == 'present_2022' & present_post == 'present_2023') ~ 'Present_2022&2023',
+        (present_pre == 'present_2022' & present_post == 'absent_2023') ~ 'Loss 2023',
+        (present_pre == 'absent_2022' & present_post == 'present_2023') ~ 'Gain 2023', TRUE ~ NA_character_
+      ))
+      #this results in some NA's as there is some sites (2 that are only sampled post)
+      df<-df %>% filter(!HBRC_Site_Name=='Wharerangi - Dan and Lindsay Bates') %>% filter(!HBRC_Site_Name=='Mangaone Rv at Dartmoor Br')%>% filter(!present=='NA')
+    }
+    if (input$integer == "2023 (Post cyclone) vs 2024") {
+      filtered_2024<-test_filtered_select %>% filter(Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
+      filtered_2023<-test_filtered_select %>% filter(Year==2023) %>% select(HBRC_Site_Name) %>% distinct()
+      all_sites_2024<-test %>% filter(Year==2024) %>% select(HBRC_Site_Name) %>% distinct()
+      all_sites_2023<-test %>% filter(Year==2023) %>% select(HBRC_Site_Name) %>% distinct()
+      pre_df<-all_sites_2023 %>% mutate(present_pre = case_when(HBRC_Site_Name %in% filtered_2023$HBRC_Site_Name~"present_2023",TRUE ~ 'absent_2023'))
+      post_df<-all_sites_2024 %>% mutate(present_post = case_when(HBRC_Site_Name %in% filtered_2024$HBRC_Site_Name~"present_2024",TRUE ~ 'absent_2024'))
+      df<-full_join(pre_df,post_df,by='HBRC_Site_Name')
+      df<-df %>% mutate(present = case_when(
+        (present_pre == 'absent_2023' & present_post == 'absent_2024') ~ 'Absent_2023&2024',
+        (present_pre == 'present_2023' & present_post == 'present_2024') ~ 'Present_2023&2024',
+        (present_pre == 'present_2023' & present_post == 'absent_2024') ~ 'Loss 2024',
+        (present_pre == 'absent_2023' & present_post == 'present_2024') ~ 'Gain 2024', TRUE ~ NA_character_
+      ))
+      #this results in some NA's as there is some sites (2 that are only sampled post)
+      df<-df %>% filter(!HBRC_Site_Name=='Wharerangi - Dan and Lindsay Bates') %>% filter(!HBRC_Site_Name=='Mangaone Rv at Dartmoor Br') %>% filter(!present=='NA')
+    }
+    
+    
   return(left_join(df,meta_data_map,by='HBRC_Site_Name'))
   })
   
--- testingshinycode.R
+++ testingshinycode.R
@@ -21,3 +21,22 @@ leaflet(x) %>%
  # addLegend("bottomright", pal = y, values = ~present,
       #      title = "Presence/absence",
       #      opacity = 1)
+
+
+pre_filtered<-test_filtered_select %>% filter(Year==2021|Year==2022 ) %>% select(HBRC_Site_Name) %>% distinct()
+post_filtered<-test_filtered_select %>% filter(Year==2023|Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
+pre_all_sites<-test %>% filter(Year==2021|Year==2022 ) %>% select(HBRC_Site_Name) %>% distinct()
+post_all_sites<-test %>% filter(Year==2023|Year==2024 ) %>% select(HBRC_Site_Name) %>% distinct()
+pre_df<-pre_all_sites %>% mutate(present_pre = case_when(HBRC_Site_Name %in% pre_filtered$HBRC_Site_Name~"present_pre",TRUE ~ 'absent_pre'))
+post_df<-post_all_sites %>% mutate(present_post = case_when(HBRC_Site_Name %in% post_filtered$HBRC_Site_Name~"present_post",TRUE ~ 'absent_post'))
+df<-full_join(pre_df,post_df,by='HBRC_Site_Name')
+df<-df %>% mutate(present = case_when(
+  (present_pre == 'absent_pre' & present_post == 'absent_post') ~ 'Absent_pre&post',
+  (present_pre == 'present_pre' & present_post == 'present_post') ~ 'Present_pre&post',
+  (present_pre == 'present_pre' & present_post == 'absent_post') ~ 'Loss post',
+  (present_pre == 'absent_pre' & present_post == 'present_post') ~ 'Gain post', TRUE ~ NA_character_
+))
+
+df<-df %>% filter(!HBRC_Site_Name=='Wharerangi - Dan and Lindsay Bates') %>% filter(!HBRC_Site_Name=='Mangaone Rv at Dartmoor Br')
+
+df %>% filter(!HBRC_Site_Name=='Mangaone Rv at Dartmoor Br')
